92a8962bf84ad63dba0c0fc8f8dcd25b820182e8
js02sixty <js02sixty@gmail.com>
2015-08-09 14:40:56 -0400

    Aug10 Update

--- src/app/index.route.js
+++ src/app/index.route.js
@@ -6,16 +6,25 @@
     .config(routeConfig);
 
   /** @ngInject */
-  function routeConfig($stateProvider, $urlRouterProvider) {
+  function routeConfig($stateProvider, $urlRouterProvider, $locationProvider) {
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
+      })
+      .state('test', {
+        url: '/test',
+        templateUrl: 'app/test/test.html',
+      })
+      .state('test2', {
+        url: '/test2',
+        templateUrl: 'app/test/test2.html',
       });
 
     $urlRouterProvider.otherwise('/');
+    $locationProvider.html5Mode(true);
   }
 
 })();
